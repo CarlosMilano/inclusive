9e2c59f2ad897a7bf9a02bc89cec065125c42b09
CarlosMilano <93209499+CarlosMilano@users.noreply.github.com>
2024-03-19 12:39:20 -0600

    Add Utilidad Reporte

--- components/Anual.tsx
+++ components/Anual.tsx
@@ -1,3 +1,35 @@
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
+
 export default function Anual() {
-  return <></>;
+  return (
+    <main>
+      <h1>Anual</h1>
+      <Table>
+        <TableHeader>
+          <TableColumn>Nombre</TableColumn>
+          <TableColumn>Apellido</TableColumn>
+          <TableColumn>Edad</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>John</TableCell>
+            <TableCell>Doe</TableCell>
+            <TableCell>30</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Jane</TableCell>
+            <TableCell>Doe</TableCell>
+            <TableCell>25</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
 }
--- pages/reportes/index.tsx
+++ pages/reportes/index.tsx
@@ -3,12 +3,94 @@ import { useEffect, useState } from "react";
 import { Box, CircularProgress } from "@mui/material";
 import Anual from "@/components/Anual";
 import Mensual from "@/components/Mensual";
+import Ventas from "@/components/Ventas";
 import { Select, SelectItem } from "@nextui-org/select";
+import supabase from "../api/supabase";
 
-export default function Home() {
+export default function Reportes() {
   const [loading, setLoading] = useState(true);
-  const [reporte, setReporte] = useState("Ventas");
+  const [reporte, setReporte] = useState("Utilidad");
   const [tipo, setTipo] = useState("Anual");
+  const [ventasData, setVentasData] = useState<any[]>([]);
+  const [years, setYears] = useState<number[]>([]);
+  const [anio, setAnio] = useState("");
+
+  useEffect(() => {
+    async function fetchVentasData() {
+      try {
+        const { data: viajesData, error: viajesError } = await supabase
+          .from("viaje")
+          .select("*");
+        const { data: proveedoresData, error: proveedoresError } =
+          await supabase.from("viajeproveedor").select("*");
+
+        if (viajesError) throw viajesError;
+        if (proveedoresError) throw proveedoresError;
+
+        const uniqueYears = Array.from(
+          new Set(
+            viajesData
+              .filter(
+                (viaje: any) => viaje.fechafactura && viaje.fechafactura !== ""
+              )
+              .map((viaje: any) => new Date(viaje.fechafactura).getFullYear())
+          )
+        ).sort((a, b) => a - b);
+        setYears(uniqueYears);
+
+        const ventasPorMesAnio = viajesData.reduce((acc: any, viaje: any) => {
+          if (viaje.fechafactura && viaje.fechafactura !== "") {
+            const fecha = new Date(viaje.fechafactura);
+            const mes = fecha.getMonth() + 1;
+            const anio = fecha.getFullYear();
+            let tarifaViaje = viaje.tarifa;
+            if (viaje.dolares && viaje.tipodecambio) {
+              tarifaViaje *= viaje.tipodecambio;
+            }
+
+            const proveedoresDelViaje = proveedoresData.filter(
+              (proveedor: any) => proveedor.viaje_id === viaje.id
+            );
+            let tarifaProveedores = 0;
+            proveedoresDelViaje.forEach((proveedor: any) => {
+              let tarifaProveedor = proveedor.tarifa;
+              if (proveedor.dolares && viaje.tipodecambio) {
+                tarifaProveedor *= viaje.tipodecambio;
+              }
+              tarifaProveedores += tarifaProveedor;
+            });
+
+            const comision = viaje.comision;
+            const utilidad = tarifaViaje - comision - tarifaProveedores;
+
+            if (!acc[mes]) {
+              acc[mes] = { mes, [anio]: utilidad };
+            } else {
+              if (!acc[mes][anio]) {
+                acc[mes][anio] = utilidad;
+              } else {
+                acc[mes][anio] += utilidad;
+              }
+            }
+          }
+          return acc;
+        }, {});
+
+        const ventasArray = Object.keys(ventasPorMesAnio).map((mes) => ({
+          mes: parseInt(mes, 10),
+          ...ventasPorMesAnio[mes],
+        }));
+
+        setVentasData(ventasArray);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    }
+
+    fetchVentasData();
+  }, []);
 
   setTimeout(() => {
     setLoading(false);
@@ -20,6 +102,10 @@ export default function Home() {
 
   const handleReporte = (value: any) => {
     setReporte(value);
+  };
+
+  const handleAnio = (value: any) => {
+    setAnio(value);
   };
 
   return (
@@ -39,41 +125,46 @@ export default function Home() {
           <CircularProgress />
         </Box>
       ) : (
-        <main className="flex flex-col h-screen mt-[60px]">
+        <main className="flex  flex-col h-screen mt-[60px]">
           <h1 className="text-4xl font-bold p-8">Reportes</h1>
-          <section className="bg-white p-2 m-3 rounded-md shadow-sm flex flex-wrap gap-2 ">
+          <section className=" bg-white p-2 m-3 rounded-md shadow-sm flex flex-wrap gap-2 ">
             <article className="w-[200px]">
               <Select
                 label="Reporte"
                 radius="sm"
-                placeholder="Selecciona un Reporte"
+                placeholder={reporte}
                 value={reporte}
               >
-                <SelectItem key="1" onClick={() => handleReporte("Ventas")}>
-                  Ventas
+                <SelectItem key="1" onClick={() => handleReporte("Utilidad")}>
+                  Utilidad
                 </SelectItem>
                 <SelectItem key="2" onClick={() => handleReporte("UxC")}>
                   Utilidad x Cliente
                 </SelectItem>
                 <SelectItem
                   key="3"
-                  onClick={() => handleReporte("Proveedores")}
+                  onClick={() => handleReporte("Proovedores")}
                 >
                   Proveedores
                 </SelectItem>
-                <SelectItem key="4" onClick={() => handleReporte("Utilidad")}>
-                  Utilidad
+                <SelectItem key="4" onClick={() => handleReporte("Ventas")}>
+                  Ventas
                 </SelectItem>
               </Select>
             </article>
-            {reporte !== "Ventas" && (
+            {reporte !== "Utilidad" && (
               <article className="w-[200px]">
                 <Select
                   label="Tipo"
                   radius="sm"
+                  value={tipo}
                   placeholder="Selecciona un Tipo"
                 >
-                  <SelectItem key="1" onClick={() => handleTipo("Anual")}>
+                  <SelectItem
+                    key="1"
+                    onClick={() => handleTipo("Anual")}
+                    value="Anual"
+                  >
                     Anual
                   </SelectItem>
                   <SelectItem key="2" onClick={() => handleTipo("Mensual")}>
@@ -84,10 +175,24 @@ export default function Home() {
             )}
             {tipo === "Mensual" && (
               <article className="w-[200px]">
-                <Select label="Año" radius="sm" placeholder="Selecciona un Año">
-                  <SelectItem key="1">2022</SelectItem>
+                <Select
+                  label="Año"
+                  placeholder="Selecciona un Año"
+                  radius="sm"
+                  value={anio}
+                >
+                  {years.map((year) => (
+                    <SelectItem key={year} onClick={() => handleAnio(year)}>
+                      {year}
+                    </SelectItem>
+                  ))}
                 </Select>
               </article>
+            )}
+          </section>
+          <section className="m-3">
+            {reporte === "Utilidad" && (
+              <Ventas data={ventasData} years={years} />
             )}
           </section>
         </main>
