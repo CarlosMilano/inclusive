958fcca2edc198e704a27e8f30c620d64b29828f
CarlosMilano <93209499+CarlosMilano@users.noreply.github.com>
2024-03-20 18:31:50 -0600

    text

--- components/Resumen.tsx
+++ components/Resumen.tsx
@@ -10,13 +10,13 @@ export default function Resumen(props: ResumenProps) {
   return (
     <>
       <section className="bg-white p-3 w-[160px] md:w-[215px] shadow-sm rounded-[14px] space-y-2">
-        <h1 className="text-lg text-gray-500 font-bold">
+        <h1 className="text-xl text-gray-500 font-bold">
           {props.title || "Titulo"}
         </h1>
         {props.loading ? (
           <Skeleton width="100%" height={50} animation="wave" />
         ) : (
-          <h2 className="text-xl md:text-2xl ">{props.monto}</h2>
+          <h2 className="text-lg md:text-2xl ">{props.monto}</h2>
         )}
       </section>
     </>
