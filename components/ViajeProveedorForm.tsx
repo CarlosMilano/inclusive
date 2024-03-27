a8c5b293efb4903f713ff5bea29e8533b7167e7e
CarlosMilano <93209499+CarlosMilano@users.noreply.github.com>
2024-03-26 23:09:16 -0600

    Add new tables

--- components/ViajeProveedorForm.tsx
+++ components/ViajeProveedorForm.tsx
@@ -652,7 +652,7 @@ export const ViajeForm = (props: ViajeFormProps) => {
                 Detalle Viaje
               </h2>
               <section className="flex flex-wrap">
-                <article className="p-2 md:w-[25%]">
+                <article className="p-2 w-full md:w-[25%]">
                   <Select
                     name="cliente_id"
                     value={viajeData.cliente_id ?? ""}
