6fa8059e2d45c738530474c696a5b5d8d628454e
CarlosMilano <carlosdavid3000@outlook.com>
2025-01-16 19:53:10 -0600

    new color depending on the client pay

--- components/CardComision.tsx
+++ components/CardComision.tsx
@@ -1,153 +1,159 @@
-import currencyFormatter from "currency-formatter";
-import PaidIcon from "@mui/icons-material/Paid";
-import { InputField } from "./InputField";
-import { useState } from "react";
-import supabase from "@/pages/api/supabase";
-import { useRouter } from "next/router";
+import currencyFormatter from "currency-formatter"
+import PaidIcon from "@mui/icons-material/Paid"
+import { InputField } from "./InputField"
+import { useState } from "react"
+import supabase from "@/pages/api/supabase"
+import { useRouter } from "next/router"
 
 interface CardComision {
-  comision: number;
-  abonocomision: number;
-  id: string;
-  factura: string;
-  onClick?: (rowData: { id: string }) => void;
+  comision: number
+  abonocomision: number
+  id: string
+  factura: string
+  onClick?: (rowData: { id: string }) => void
+  tarifa: number
+  abonado: number
 }
 
 export default function CardComision(props: CardComision) {
-  const [openCobradoTarifaViaje, setOpenCobradoTarifaViaje] = useState(false);
-  const [openAbonadoTarifaViaje, setOpenAbonadoTarifaViaje] = useState(false);
-  const [nuevoAbonoTarifaViaje, setNuevoAbonoTarifaViaje] = useState(0);
-  const router = useRouter();
+  const [openCobradoTarifaViaje, setOpenCobradoTarifaViaje] = useState(false)
+  const [openAbonadoTarifaViaje, setOpenAbonadoTarifaViaje] = useState(false)
+  const [nuevoAbonoTarifaViaje, setNuevoAbonoTarifaViaje] = useState(0)
+  const router = useRouter()
   const handleRowClick = (rowData: { id: string }) => {
     if (props.onClick) {
-      props.onClick(rowData);
+      props.onClick(rowData)
     }
-  };
+  }
 
   const clickCobradoTarifaViaje = () => {
-    setOpenCobradoTarifaViaje(!openCobradoTarifaViaje);
-  };
+    setOpenCobradoTarifaViaje(!openCobradoTarifaViaje)
+  }
 
   const clickAbonadoTarifaViaje = () => {
-    setOpenAbonadoTarifaViaje(!openAbonadoTarifaViaje);
-  };
+    setOpenAbonadoTarifaViaje(!openAbonadoTarifaViaje)
+  }
 
-  const handleAbonoTarifaViajeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNuevoAbonoTarifaViaje(parseInt(event.target.value));
-  };
+  const handleAbonoTarifaViajeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNuevoAbonoTarifaViaje(parseInt(event.target.value))
+  }
 
   const contadoTarifaViaje = async () => {
     try {
       await supabase
         .from("viaje")
         .update({ abonocomision: props.comision })
-        .eq("id", props.id);
-      router.reload();
+        .eq("id", props.id)
+      router.reload()
     } catch (error) {
-      console.error("Error al cobrar tarifa del viaje:", error);
+      console.error("Error al cobrar tarifa del viaje:", error)
     }
-  };
+  }
 
   const abonoTarifaViaje = async () => {
     try {
       await supabase
         .from("viaje")
         .update({ abonocomision: nuevoAbonoTarifaViaje })
-        .eq("id", props.id);
-      router.reload();
+        .eq("id", props.id)
+      router.reload()
     } catch (error) {
-      console.error("Error al abonar a la tarifa del viaje:", error);
+      console.error("Error al abonar a la tarifa del viaje:", error)
     }
-  };
+  }
 
   return (
-    <main className="flex flex-col m-2 transition-all cursor-pointer duration-300 relative">
-      <article className="flex flex-col items-center justify-center absolute top-[52px] left-56">
-                  {props.comision !== props.abonocomision && (
-                    <PaidIcon
-                      onClick={clickCobradoTarifaViaje}
-                      sx={{ color: "#2f9e44", cursor: "pointer" }}
-                    />
-                  )}
-                  <section
-                    className={`flex flex-col rounded-2xl shadow-sm z-20 w-[210px] p-3 bg-white transition-all duration-300${
-                      openCobradoTarifaViaje
-                        ? " scale-100 opacity-100 ease-out"
-                        : " scale-50 opacity-0 ease-in"
-                    }`}
-                    style={{
-                      visibility: openCobradoTarifaViaje ? "visible" : "hidden",
-                    }}
-                  >
-                    <article className="flex space-x-3">
-                      <button
-                        className="py-2 bg-blue-600 text-lg  text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={contadoTarifaViaje}
-                      >
-                        Contado
-                      </button>
-                      <button
-                        className="py-2 bg-blue-600 text-lg text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={clickAbonadoTarifaViaje}
-                      >
-                        Abono
-                      </button>
-                    </article>
+    <main className='flex flex-col m-2 transition-all cursor-pointer duration-300 relative'>
+      <article className='flex flex-col items-center justify-center absolute top-[52px] left-56'>
+        {props.comision !== props.abonocomision && (
+          <PaidIcon
+            onClick={clickCobradoTarifaViaje}
+            sx={{
+              color: props.tarifa === props.abonado ? "#dc3545" : "#2f9e44",
+              cursor: "pointer"
+            }}
+          />
+        )}
+        <section
+          className={`flex flex-col rounded-2xl shadow-sm z-20 w-[210px] p-3 bg-white transition-all duration-300${
+            openCobradoTarifaViaje
+              ? " scale-100 opacity-100 ease-out"
+              : " scale-50 opacity-0 ease-in"
+          }`}
+          style={{
+            visibility: openCobradoTarifaViaje ? "visible" : "hidden"
+          }}
+        >
+          <article className='flex space-x-3'>
+            <button
+              className='py-2 bg-blue-600 text-lg  text-white shadow-sm rounded-lg w-[90px]'
+              onClick={contadoTarifaViaje}
+            >
+              Contado
+            </button>
+            <button
+              className='py-2 bg-blue-600 text-lg text-white shadow-sm rounded-lg w-[90px]'
+              onClick={clickAbonadoTarifaViaje}
+            >
+              Abono
+            </button>
+          </article>
 
-                    <section
-                      className={`transition-all flex flex-col items-center duration-300 overflow-hidden${
-                        openAbonadoTarifaViaje
-                          ? " max-h-[500px] opacity-100 p-2"
-                          : " max-h-0 opacity-50"
-                      }`}
-                      style={{
-                        visibility: openAbonadoTarifaViaje
-                          ? "visible"
-                          : "hidden",
-                      }}
-                    >
-                      <InputField
-                        name="abonado"
-                        value={nuevoAbonoTarifaViaje}
-                        onChange={handleAbonoTarifaViajeChange}
-                        type="number"
-                      />
+          <section
+            className={`transition-all flex flex-col items-center duration-300 overflow-hidden${
+              openAbonadoTarifaViaje
+                ? " max-h-[500px] opacity-100 p-2"
+                : " max-h-0 opacity-50"
+            }`}
+            style={{
+              visibility: openAbonadoTarifaViaje ? "visible" : "hidden"
+            }}
+          >
+            <InputField
+              name='abonado'
+              value={nuevoAbonoTarifaViaje}
+              onChange={handleAbonoTarifaViajeChange}
+              type='number'
+            />
 
-                      <button
-                        className="py-2 bg-blue-600 m-2 text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={abonoTarifaViaje}
-                      >
-                        Guardar
-                      </button>
-                    </section>
-                  </section>
-                </article>
-                <section className="shadow-sm rounded-lg p-4 w-[340px] justify-between flex flex-row bg-white m-2 items-center cursor-pointer"
-      onClick={() => handleRowClick(props)}>
-                  <article>
-        <h2 className="text-gray-400 text-xs">Factura</h2>
-        <p>{props.factura}</p>
+            <button
+              className='py-2 bg-blue-600 m-2 text-white shadow-sm rounded-lg w-[90px]'
+              onClick={abonoTarifaViaje}
+            >
+              Guardar
+            </button>
+          </section>
+        </section>
       </article>
-      <article className="text-sm flex flex-col">
-        <h2 className="text-gray-400 text-xs">Comisión</h2>
-        <p>
-          {currencyFormatter.format(props.comision, {
-            code: "MXN",
-            precision: 0,
-          })}
-        </p>
-      </article>
-      <article className="text-sm flex flex-col">
-        <h2 className="text-gray-400 text-xs">Abonado</h2>
-        <p>
-          {currencyFormatter.format(props.abonocomision, {
-            code: "MXN",
-            precision: 0,
-          })}
-        </p>
-      </article>
-                </section>
-      
+      <section
+        className='shadow-sm rounded-lg p-4 w-[340px] justify-between flex flex-row bg-white m-2 items-center cursor-pointer'
+        onClick={() => handleRowClick(props)}
+      >
+        <article>
+          <h2 className='text-gray-400 text-xs'>Factura</h2>
+          <p>{props.factura}</p>
+        </article>
+        <article className='text-sm flex flex-col'>
+          <h2 className='text-gray-400 text-xs'>Comisión</h2>
+          <p>
+            {currencyFormatter.format(props.comision, {
+              code: "MXN",
+              precision: 0
+            })}
+          </p>
+        </article>
+        <article className='text-sm flex flex-col'>
+          <h2 className='text-gray-400 text-xs'>Abonado</h2>
+          <p>
+            {currencyFormatter.format(props.abonocomision, {
+              code: "MXN",
+              precision: 0
+            })}
+          </p>
+        </article>
+      </section>
     </main>
-  );
+  )
 }
--- pages/comision/[id]/index.tsx
+++ pages/comision/[id]/index.tsx
@@ -1,26 +1,25 @@
-import Head from "next/head";
-import { useState, useEffect } from "react";
-import supabase from "../../api/supabase";
-import { useRouter } from "next/router";
-import Table from "@/components/Table";
-import { addDays, differenceInDays, parseISO } from "date-fns";
-import Pagination from "@mui/material/Pagination";
-import { Box, CircularProgress } from "@mui/material";
-import CardComision from "@/components/CardComision";
-import { Viaje } from "@/types/Viaje";
-import { Cliente } from "@/types/Cliente";
-
+import Head from "next/head"
+import { useState, useEffect } from "react"
+import supabase from "../../api/supabase"
+import { useRouter } from "next/router"
+import Table from "@/components/Table"
+import { addDays, differenceInDays, parseISO } from "date-fns"
+import Pagination from "@mui/material/Pagination"
+import { Box, CircularProgress } from "@mui/material"
+import CardComision from "@/components/CardComision"
+import { Viaje } from "@/types/Viaje"
+import { Cliente } from "@/types/Cliente"
 
 export default function Home() {
-  const [viajes, setViajes] = useState<Viaje[]>([]);
-  const [cliente, setCliente] = useState<Cliente>();
-  const router = useRouter();
-  const { id } = router.query;
-  const [loading, setLoading] = useState(true);
+  const [viajes, setViajes] = useState<Viaje[]>([])
+  const [cliente, setCliente] = useState<Cliente>()
+  const router = useRouter()
+  const { id } = router.query
+  const [loading, setLoading] = useState(true)
 
   const filterViajes = viajes.filter(
-    (viaje) => viaje.comision !== viaje.abonocomision
-  );
+    viaje => viaje.comision !== viaje.abonocomision
+  )
 
   useEffect(() => {
     const fetchClienteData = async () => {
@@ -29,44 +28,44 @@ export default function Home() {
           .from("cliente")
           .select("*")
           .eq("id", id)
-          .single();
+          .single()
 
         if (clienteError) {
-          console.error(clienteError);
+          console.error(clienteError)
         } else {
-          setCliente(clienteData);
+          setCliente(clienteData)
           setTimeout(() => {
-            setLoading(false);
-          }, 1500);
+            setLoading(false)
+          }, 1500)
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
     const fetchData = async () => {
       try {
         const { data: viajesData, error: viajesError } = await supabase
           .from("viaje")
           .select("*")
-          .eq("cliente_id", id);
-        if (viajesError) console.error(viajesError);
+          .eq("cliente_id", id)
+        if (viajesError) console.error(viajesError)
         else {
           const viajesConDiasCredito = viajesData.map((viaje: Viaje) => ({
             ...viaje,
-            diasCredito: cliente?.diascredito || 0,
-          }));
-          setViajes(viajesConDiasCredito || []);
+            diasCredito: cliente?.diascredito || 0
+          }))
+          setViajes(viajesConDiasCredito || [])
         }
         setTimeout(() => {
-          setLoading(false);
-        }, 1000);
+          setLoading(false)
+        }, 1000)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
-    fetchClienteData();
-    fetchData();
-  }, [id]);
+    }
+    fetchClienteData()
+    fetchData()
+  }, [id])
 
   return (
     <>
@@ -79,18 +78,18 @@ export default function Home() {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: "100vh",
+            height: "100vh"
           }}
         >
           <CircularProgress />
         </Box>
       ) : (
-        <main className="flex flex-col h-screen mt-[60px]">
-          <section className="p-8">
-            <h1 className="text-4xl font-bold">Comisión {cliente?.nombre}</h1>
+        <main className='flex flex-col h-screen mt-[60px]'>
+          <section className='p-8'>
+            <h1 className='text-4xl font-bold'>Comisión {cliente?.nombre}</h1>
           </section>
-          <section className="flex flex-wrap justify-center">
-            {filterViajes.map((viaje) => (
+          <section className='flex flex-wrap justify-center'>
+            {filterViajes.map(viaje => (
               <CardComision
                 key={viaje.id}
                 comision={viaje.comision}
@@ -98,11 +97,13 @@ export default function Home() {
                 id={viaje.id}
                 onClick={() => router.push(`/viaje/${viaje.id}`)}
                 factura={viaje.factura}
+                tarifa={viaje.tarifa}
+                abonado={viaje.abonado}
               />
             ))}
           </section>
         </main>
       )}
     </>
-  );
+  )
 }
