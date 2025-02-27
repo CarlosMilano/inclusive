52790d100dd186d79f5e6aed07528c720e2db975
CarlosMilano <carlosdavid3000@outlook.com>
2025-02-26 19:01:16 -0600

    fix utilidad

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,152 +1,148 @@
-import currencyFormatter from "currency-formatter";
-import PaidIcon from "@mui/icons-material/Paid";
-import { InputField } from "./InputField";
-import { useState } from "react";
-import supabase from "@/pages/api/supabase";
-import { useRouter } from "next/router";
+import currencyFormatter from "currency-formatter"
+import PaidIcon from "@mui/icons-material/Paid"
+import { InputField } from "./InputField"
+import { useState } from "react"
+import supabase from "@/pages/api/supabase"
+import { useRouter } from "next/router"
 
 interface TableProps {
-  origen: string;
-  destino: string;
-  monto: number;
-  factura?: string;
-  referencia?: string;
-  id: string;
-  idproveedor?: string;
-  fechafactura: string;
-  diasRestantes: number;
-  onClick?: (rowData: { id: string }) => void;
-  historial?: boolean;
-  folio?: number;
-  dolares: boolean;
-  tipodecambio: number;
-  comision?: number;
-  tarifaProveedor?: number;
-  utilidad?: boolean;
-  abonado?: number;
-  isCliente?: boolean;
+  origen: string
+  destino: string
+  monto: number
+  factura?: string
+  referencia?: string
+  id: string
+  idproveedor?: string
+  fechafactura: string
+  diasRestantes: number
+  onClick?: (rowData: { id: string }) => void
+  historial?: boolean
+  folio?: number
+  dolares: boolean
+  tipodecambio: number
+  comision?: number
+  tarifaProveedor?: number
+  utilidad?: boolean
+  abonado?: number
+  isCliente?: boolean
 }
 
 export default function Table(props: TableProps) {
-  const [openCobradoTarifaViaje, setOpenCobradoTarifaViaje] = useState(false);
-  const [openAbonadoTarifaViaje, setOpenAbonadoTarifaViaje] = useState(false);
-  const [nuevoAbonoTarifaViaje, setNuevoAbonoTarifaViaje] = useState(0);
-  const router = useRouter();
+  const [openCobradoTarifaViaje, setOpenCobradoTarifaViaje] = useState(false)
+  const [openAbonadoTarifaViaje, setOpenAbonadoTarifaViaje] = useState(false)
+  const [nuevoAbonoTarifaViaje, setNuevoAbonoTarifaViaje] = useState(0)
+  const router = useRouter()
   const handleRowClick = (rowData: { id: string }) => {
     if (props.onClick) {
-      props.onClick(rowData);
+      props.onClick(rowData)
     }
-  };
+  }
 
   const clickCobradoTarifaViaje = () => {
-    setOpenCobradoTarifaViaje(!openCobradoTarifaViaje);
-  };
+    setOpenCobradoTarifaViaje(!openCobradoTarifaViaje)
+  }
 
   const clickAbonadoTarifaViaje = () => {
-    setOpenAbonadoTarifaViaje(!openAbonadoTarifaViaje);
-  };
+    setOpenAbonadoTarifaViaje(!openAbonadoTarifaViaje)
+  }
 
   const contadoTarifaViaje = async () => {
-    const key = props.isCliente ? "viaje" : "viajeproveedor";
-    const id = props.isCliente ? props.id : props.idproveedor;
+    const key = props.isCliente ? "viaje" : "viajeproveedor"
+    const id = props.isCliente ? props.id : props.idproveedor
     try {
-      await supabase
-        .from(key)
-        .update({ abonado: props.monto })
-        .eq("id", id);
-      router.reload();
+      await supabase.from(key).update({ abonado: props.monto }).eq("id", id)
+      router.reload()
     } catch (error) {
-      console.error("Error al cobrar tarifa del viaje:", error);
+      console.error("Error al cobrar tarifa del viaje:", error)
     }
-  };
+  }
 
   const abonoTarifaViaje = async () => {
-    const key = props.isCliente ? "viaje" : "viajeproveedor";
-    const id = props.isCliente ? props.id : props.idproveedor;
+    const key = props.isCliente ? "viaje" : "viajeproveedor"
+    const id = props.isCliente ? props.id : props.idproveedor
     try {
       await supabase
         .from(key)
         .update({ abonado: nuevoAbonoTarifaViaje })
-        .eq("id", id);
-      router.reload();
+        .eq("id", id)
+      router.reload()
     } catch (error) {
-      console.error("Error al abonar a la tarifa del viaje:", error);
+      console.error("Error al abonar a la tarifa del viaje:", error)
     }
-  };
+  }
 
-  const handleAbonoTarifaViajeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNuevoAbonoTarifaViaje(parseInt(event.target.value));
-  };
-
+  const handleAbonoTarifaViajeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNuevoAbonoTarifaViaje(parseInt(event.target.value))
+  }
 
   const utilidadPorViaje = props.dolares
     ? props.monto * props.tipodecambio -
       (props.comision || 0) -
       (props.tarifaProveedor || 0)
-    : props.monto - (props.comision || 0) - (props.tarifaProveedor || 0);
+    : props.monto - (props.comision || 0) - (props.tarifaProveedor || 0)
 
   return (
-    <main className="flex flex-col m-2 transition-all cursor-pointer duration-300 relative">
-      <article className="flex flex-col items-center justify-center absolute top-[66px] right-[-20px]">
-                  {props.abonado !== props.monto && props.historial === false && (
-                    <PaidIcon
-                      onClick={clickCobradoTarifaViaje}
-                      sx={{ color: "#2f9e44", cursor: "pointer" }}
-                    />
-                  )}
-                  <section
-                    className={`flex flex-col rounded-2xl shadow-sm z-20 w-[210px] p-3 bg-white transition-all duration-300${
-                      openCobradoTarifaViaje
-                        ? " scale-100 opacity-100 ease-out"
-                        : " scale-50 opacity-0 ease-in"
-                    }`}
-                    style={{
-                      visibility: openCobradoTarifaViaje ? "visible" : "hidden",
-                    }}
-                  >
-                    <article className="flex space-x-3">
-                      <button
-                        className="py-2 bg-blue-600 text-lg  text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={contadoTarifaViaje}
-                      >
-                        Contado
-                      </button>
-                      <button
-                        className="py-2 bg-blue-600 text-lg text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={clickAbonadoTarifaViaje}
-                      >
-                        Abono
-                      </button>
-                    </article>
+    <main className='flex flex-col m-2 transition-all cursor-pointer duration-300 relative'>
+      <article className='flex flex-col items-center justify-center absolute top-[66px] right-[-20px]'>
+        {props.abonado !== props.monto && props.historial === false && (
+          <PaidIcon
+            onClick={clickCobradoTarifaViaje}
+            sx={{ color: "#2f9e44", cursor: "pointer" }}
+          />
+        )}
+        <section
+          className={`flex flex-col rounded-2xl shadow-sm z-20 w-[210px] p-3 bg-white transition-all duration-300${
+            openCobradoTarifaViaje
+              ? " scale-100 opacity-100 ease-out"
+              : " scale-50 opacity-0 ease-in"
+          }`}
+          style={{
+            visibility: openCobradoTarifaViaje ? "visible" : "hidden"
+          }}
+        >
+          <article className='flex space-x-3'>
+            <button
+              className='py-2 bg-blue-600 text-lg  text-white shadow-sm rounded-lg w-[90px]'
+              onClick={contadoTarifaViaje}
+            >
+              Contado
+            </button>
+            <button
+              className='py-2 bg-blue-600 text-lg text-white shadow-sm rounded-lg w-[90px]'
+              onClick={clickAbonadoTarifaViaje}
+            >
+              Abono
+            </button>
+          </article>
 
-                    <section
-                      className={`transition-all flex flex-col items-center duration-300 overflow-hidden${
-                        openAbonadoTarifaViaje
-                          ? " max-h-[500px] opacity-100 p-2"
-                          : " max-h-0 opacity-50"
-                      }`}
-                      style={{
-                        visibility: openAbonadoTarifaViaje
-                          ? "visible"
-                          : "hidden",
-                      }}
-                    >
-                      <InputField
-                        name="abonado"
-                        value={nuevoAbonoTarifaViaje}
-                        onChange={handleAbonoTarifaViajeChange}
-                        type="number"
-                      />
+          <section
+            className={`transition-all flex flex-col items-center duration-300 overflow-hidden${
+              openAbonadoTarifaViaje
+                ? " max-h-[500px] opacity-100 p-2"
+                : " max-h-0 opacity-50"
+            }`}
+            style={{
+              visibility: openAbonadoTarifaViaje ? "visible" : "hidden"
+            }}
+          >
+            <InputField
+              name='abonado'
+              value={nuevoAbonoTarifaViaje}
+              onChange={handleAbonoTarifaViajeChange}
+              type='number'
+            />
 
-                      <button
-                        className="py-2 bg-blue-600 m-2 text-white shadow-sm rounded-lg w-[90px]"
-                        onClick={abonoTarifaViaje}
-                      >
-                        Guardar
-                      </button>
-                    </section>
-                  </section>
-                </article>
+            <button
+              className='py-2 bg-blue-600 m-2 text-white shadow-sm rounded-lg w-[90px]'
+              onClick={abonoTarifaViaje}
+            >
+              Guardar
+            </button>
+          </section>
+        </section>
+      </article>
       <section
         className={`shadow-sm rounded-lg p-3 w-[355px] flex flex-row bg-white justify-between items-center focus:outline-none  
                     ${props.diasRestantes < 0 ? "hover:border-red-500" : ""} 
@@ -162,41 +158,41 @@ export default function Table(props: TableProps) {
                         `}
         onClick={() => handleRowClick(props)}
       >
-        <article className="flex flex-col text-xs ">
-          <h2 className="text-gray-400 ">Origen</h2>
-          <p className="pl-[2px]">{props.origen || "N/A"}</p>
-          <h2 className="text-gray-400 ">Destino</h2>
-          <p className="pl-[2px]">{props.destino || "N/A"}</p>
+        <article className='flex flex-col text-xs '>
+          <h2 className='text-gray-400 '>Origen</h2>
+          <p className='pl-[2px]'>{props.origen || "N/A"}</p>
+          <h2 className='text-gray-400 '>Destino</h2>
+          <p className='pl-[2px]'>{props.destino || "N/A"}</p>
         </article>
-        <article className="flex flex-row text-xs items-center justify-center">
+        <article className='flex flex-row text-xs items-center justify-center'>
           {props.factura && (
-            <article className="p-1">
-              <h2 className="text-gray-400">Factura</h2>
-              <p className="pl-[2px]">{props.factura || "N/A"}</p>
+            <article className='p-1'>
+              <h2 className='text-gray-400'>Factura</h2>
+              <p className='pl-[2px]'>{props.factura || "N/A"}</p>
             </article>
           )}
           {props.referencia && (
-            <article className="p-1">
-              <h2 className="text-gray-400">Referencia</h2>
-              <p className="pl-[2px]">{props.referencia || "N/A"}</p>
+            <article className='p-1'>
+              <h2 className='text-gray-400'>Referencia</h2>
+              <p className='pl-[2px]'>{props.referencia || "N/A"}</p>
             </article>
           )}
           {props.utilidad && (
-            <article className="p-1 ">
-              <h2 className="text-gray-400 ">Utilidad</h2>
-              <p className="pl-[2px]">
+            <article className='p-1 '>
+              <h2 className='text-gray-400 '>Utilidad</h2>
+              <p className='pl-[2px]'>
                 {currencyFormatter.format(utilidadPorViaje || 0, {
                   code: "MXN",
-                  precision: 0,
+                  precision: 0
                 })}
               </p>
             </article>
           )}
         </article>
-        <article className="text-xs flex flex-col justify-center items-end space-y-1">
+        <article className='text-xs flex flex-col justify-center items-end space-y-1'>
           {props.historial ? (
-            <div className="bg-blue-500 rounded-full w-9 h-9 flex justify-center items-center">
-              <div className="bg-white rounded-full p-1 w-8 h-8 flex justify-center items-center">
+            <div className='bg-blue-500 rounded-full w-9 h-9 flex justify-center items-center'>
+              <div className='bg-white rounded-full p-1 w-8 h-8 flex justify-center items-center'>
                 {props.folio || "N/A"}
               </div>
             </div>
@@ -208,31 +204,31 @@ export default function Table(props: TableProps) {
                 props.diasRestantes < 0 ? "bg-red-500" : ""
               }`}
             >
-              <div className="bg-white rounded-full p-1 w-7 h-7 flex justify-center items-center">
+              <div className='bg-white rounded-full p-1 w-7 h-7 flex justify-center items-center'>
                 {props.diasRestantes >= 0
                   ? props.diasRestantes
                   : `-${Math.abs(props.diasRestantes)}`}
               </div>
             </div>
           )}
-        <h2 className="text-gray-400 ">Tarifa</h2>
+          <h2 className='text-gray-400 '>Tarifa</h2>
           {props.dolares ? (
-            <p className="text-sm pr-[2px]">
+            <p className='text-sm pr-[2px]'>
               {currencyFormatter.format(props.monto * props.tipodecambio, {
                 code: "MXN",
-                precision: 0,
+                precision: 0
               })}
             </p>
           ) : (
-            <p className="text-sm pr-[2px]">
+            <p className='text-sm pr-[2px]'>
               {currencyFormatter.format(props.monto, {
                 code: "MXN",
-                precision: 0,
+                precision: 0
               })}
             </p>
           )}
         </article>
       </section>
     </main>
-  );
+  )
 }
--- pages/cliente/[id]/index.tsx
+++ pages/cliente/[id]/index.tsx
@@ -1,50 +1,49 @@
-import Head from "next/head";
-import { useState, useEffect } from "react";
-import supabase from "../../api/supabase";
-import { useRouter } from "next/router";
-import Table from "@/components/Table";
-import { addDays, differenceInDays, parseISO } from "date-fns";
-import { Box, CircularProgress } from "@mui/material";
-import currencyFormatter from "currency-formatter";
-import { Viaje } from "@/types/Viaje";
-import { ViajeProveedor } from "@/types/ViajeProveedor";
-import { Cliente } from "@/types/Cliente";
-
+import Head from "next/head"
+import { useState, useEffect } from "react"
+import supabase from "../../api/supabase"
+import { useRouter } from "next/router"
+import Table from "@/components/Table"
+import { addDays, differenceInDays, parseISO } from "date-fns"
+import { Box, CircularProgress } from "@mui/material"
+import currencyFormatter from "currency-formatter"
+import { Viaje } from "@/types/Viaje"
+import { ViajeProveedor } from "@/types/ViajeProveedor"
+import { Cliente } from "@/types/Cliente"
 
 export default function Home() {
-  const [viajes, setViajes] = useState<Viaje[]>([]);
-  const [cliente, setCliente] = useState<Cliente>();
-  const [proveedor, setProveedor] = useState<ViajeProveedor[]>([]);
-  const router = useRouter();
-  const { id } = router.query;
-  const [loading, setLoading] = useState(true);
-  const today = new Date();
+  const [viajes, setViajes] = useState<Viaje[]>([])
+  const [cliente, setCliente] = useState<Cliente>()
+  const [proveedor, setProveedor] = useState<ViajeProveedor[]>([])
+  const router = useRouter()
+  const { id } = router.query
+  const [loading, setLoading] = useState(true)
+  const today = new Date()
 
-  const filterViajes = viajes.filter((viaje) => viaje.tarifa !== viaje.abonado);
+  const filterViajes = viajes.filter(viaje => viaje.tarifa !== viaje.abonado)
 
   const sortedViajes = filterViajes.sort((viajeA, viajeB) => {
     const fechaLimiteA = viajeA.fechafactura
       ? addDays(parseISO(viajeA.fechafactura), cliente?.diascredito || 0)
-      : null;
+      : null
 
     const diasRestantesA = fechaLimiteA
       ? differenceInDays(fechaLimiteA, today)
-      : null;
+      : null
 
     const fechaLimiteB = viajeB.fechafactura
       ? addDays(parseISO(viajeB.fechafactura), cliente?.diascredito || 0)
-      : null;
+      : null
 
     const diasRestantesB = fechaLimiteB
       ? differenceInDays(fechaLimiteB, today)
-      : null;
+      : null
 
     if (diasRestantesA === null || diasRestantesB === null) {
-      return diasRestantesA === null ? 1 : -1;
+      return diasRestantesA === null ? 1 : -1
     }
 
-    return diasRestantesA - diasRestantesB;
-  });
+    return diasRestantesA - diasRestantesB
+  })
 
   useEffect(() => {
     const fetchClienteData = async () => {
@@ -53,122 +52,115 @@ export default function Home() {
           .from("cliente")
           .select("*")
           .eq("id", id)
-          .single();
+          .single()
 
         if (clienteError) {
-          console.error(clienteError);
+          console.error(clienteError)
         } else {
-          setCliente(clienteData);
+          setCliente(clienteData)
           setTimeout(() => {
-            setLoading(false);
-          }, 1500);
+            setLoading(false)
+          }, 1500)
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
     const fetchData = async () => {
       try {
         const { data: viajesData, error: viajesError } = await supabase
           .from("viaje")
           .select("*")
           .eq("cliente_id", id)
-          .order("fechafactura", { ascending: false });
-        if (viajesError) console.error(viajesError);
+          .order("fechafactura", { ascending: false })
+        if (viajesError) console.error(viajesError)
         else {
           const viajesConDiasCredito = viajesData.map((viaje: Viaje) => ({
             ...viaje,
-            diasCredito: cliente?.diascredito || 0,
-          }));
-          setViajes(viajesConDiasCredito || []);
+            diasCredito: cliente?.diascredito || 0
+          }))
+          setViajes(viajesConDiasCredito || [])
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
     const fetchDataProveedor = async () => {
       try {
         const { data: viajesDataProveedor, error: viajesError } = await supabase
           .from("viajeproveedor")
-          .select("*");
-        if (viajesError) console.error(viajesError);
-        else {
-          setProveedor(viajesDataProveedor || []);
-        }
+          .select("*, viaje!inner(*)")
+          .eq("viaje.cliente_id", id)
+
+        if (viajesError) throw viajesError
+        setProveedor(viajesDataProveedor || [])
       } catch (error) {
-        console.error(error);
+        console.error("Error en fetchDataProveedor:", error)
       }
-    };
-    fetchClienteData();
-    fetchData();
-    fetchDataProveedor();
-  }, [id]);
+    }
+    fetchClienteData()
+    fetchData()
+    fetchDataProveedor()
+  }, [id])
 
   const calcularSumaTarifasProveedor = (viajeId: string) => {
     const viajesProveedorRelacionados = proveedor.filter(
-      (viajeProveedor) => viajeProveedor.viaje_id === viajeId
-    );
+      viajeProveedor => viajeProveedor.viaje_id === viajeId
+    )
 
     const sumaTarifasProveedor = viajesProveedorRelacionados.reduce(
       (acc, viajeProveedor) => {
         const viajeCorrespondiente = viajes.find(
-          (viaje) => viaje.id === viajeProveedor.viaje_id
-        );
+          viaje => viaje.id === viajeProveedor.viaje_id
+        )
 
         if (
           viajeCorrespondiente &&
           viajeProveedor.dolares &&
           viajeCorrespondiente.tipodecambio
         ) {
-          return (
-            acc + viajeProveedor.tarifa * viajeCorrespondiente.tipodecambio
-          );
+          return acc + viajeProveedor.tarifa * viajeCorrespondiente.tipodecambio
         } else {
-          return acc + viajeProveedor.tarifa;
+          return acc + viajeProveedor.tarifa
         }
       },
       0
-    );
+    )
 
-    return sumaTarifasProveedor;
-  };
+    return sumaTarifasProveedor
+  }
 
   const totalVencido = sortedViajes.reduce((acc, viaje) => {
     const fechaLimite = viaje.fechafactura
       ? addDays(parseISO(viaje.fechafactura), cliente?.diascredito || 0)
-      : null;
+      : null
 
-    const diasRestantes = fechaLimite
-      ? differenceInDays(fechaLimite, today)
-      : 0;
-
+    const diasRestantes = fechaLimite ? differenceInDays(fechaLimite, today) : 0
 
     if (viaje.fechafactura && viaje.dolares) {
-      return diasRestantes < 0 ? acc + viaje.tarifa * viaje.tipodecambio : acc;
+      return diasRestantes < 0 ? acc + viaje.tarifa * viaje.tipodecambio : acc
     } else if (viaje.fechafactura) {
-      return diasRestantes < 0 ? acc + viaje.tarifa : acc;
+      return diasRestantes < 0 ? acc + viaje.tarifa : acc
     }
 
-    return acc;
-  }, 0);
+    return acc
+  }, 0)
 
   const totalPorVencer = sortedViajes.reduce((acc, viaje) => {
     const fechaLimite = viaje.fechafactura
       ? addDays(parseISO(viaje.fechafactura), cliente?.diascredito || 0)
-      : null;
+      : null
 
-    const diasRestantes = fechaLimite
-      ? differenceInDays(fechaLimite, today)
-      : 0;
+    const diasRestantes = fechaLimite ? differenceInDays(fechaLimite, today) : 0
 
     if (viaje.fechafactura && viaje.dolares) {
-      return diasRestantes >= 0 ? acc + viaje.tarifa * viaje.tipodecambio : acc;
+      return diasRestantes >= 0 ? acc + viaje.tarifa * viaje.tipodecambio : acc
     } else if (viaje.fechafactura) {
-      return diasRestantes >= 0 ? acc + viaje.tarifa : acc;
+      return diasRestantes >= 0 ? acc + viaje.tarifa : acc
     }
 
-    return acc;
-  }, 0);
+    return acc
+  }, 0)
 
   return (
     <>
@@ -181,50 +173,50 @@ export default function Home() {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            height: "100vh",
+            height: "100vh"
           }}
         >
           <CircularProgress />
         </Box>
       ) : (
-        <main className="flex flex-col min-h-screen mt-[60px]">
-          <section className="p-8 flex flex-wrap items-center gap-6">
-            <h1 className="text-3xl font-bold md:text-4xl">
+        <main className='flex flex-col min-h-screen mt-[60px]'>
+          <section className='p-8 flex flex-wrap items-center gap-6'>
+            <h1 className='text-3xl font-bold md:text-4xl'>
               Viajes {cliente?.nombre}
             </h1>
-            <article className="flex flex-col items-start space-y-1 text-sm md:text-base">
-              <article className="flex justify-center items-center gap-1">
-                <div className=" bg-red-500 w-3 h-3 rounded-full" />
+            <article className='flex flex-col items-start space-y-1 text-sm md:text-base'>
+              <article className='flex justify-center items-center gap-1'>
+                <div className=' bg-red-500 w-3 h-3 rounded-full' />
                 {currencyFormatter.format(totalVencido, {
                   code: "MXN",
-                  precision: 0,
+                  precision: 0
                 })}
               </article>
-              <article className="flex justify-center items-center gap-1">
-                <div className=" bg-green-500 w-3 h-3 rounded-full" />
+              <article className='flex justify-center items-center gap-1'>
+                <div className=' bg-green-500 w-3 h-3 rounded-full' />
                 {currencyFormatter.format(totalPorVencer, {
                   code: "MXN",
-                  precision: 0,
+                  precision: 0
                 })}
               </article>
             </article>
           </section>
-          <section className="flex flex-wrap justify-center">
-            {sortedViajes.map((viaje) => {
+          <section className='flex flex-wrap justify-center'>
+            {sortedViajes.map(viaje => {
               const fechaLimite = viaje.fechafactura
                 ? addDays(
                     parseISO(viaje.fechafactura),
                     cliente?.diascredito || 0
                   )
-                : null;
+                : null
 
               const diasRestantes = fechaLimite
                 ? differenceInDays(fechaLimite, today)
-                : 0;
+                : 0
 
               const sumaTarifasProveedor = calcularSumaTarifasProveedor(
                 viaje.id
-              );
+              )
 
               return (
                 <Table
@@ -237,8 +229,8 @@ export default function Home() {
                   id={viaje.id || ""}
                   fechafactura={viaje.fechafactura || ""}
                   diasRestantes={diasRestantes}
-                  onClick={(rowData) => {
-                    router.push(`/viaje/${rowData.id}`);
+                  onClick={rowData => {
+                    router.push(`/viaje/${rowData.id}`)
                   }}
                   dolares={viaje.dolares}
                   tipodecambio={viaje.tipodecambio}
@@ -247,13 +239,13 @@ export default function Home() {
                   utilidad
                   abonado={viaje.abonado || 0}
                   isCliente
-                  historial = {false}
+                  historial={false}
                 />
-              );
+              )
             })}
           </section>
         </main>
       )}
     </>
-  );
+  )
 }
